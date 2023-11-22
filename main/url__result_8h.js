0d97c2a78ba8a0febb736b0ffd5f334461910a90
rmisev <rmisev3@gmail.com>
2023-11-22 21:10:56 +0000

    Deploying to gh-pages from @ rmisev/url_whatwg@1f95bfb684bbc728b0cea1dc7a9fc97121d5259c 🚀

--- main/url__result_8h.js
+++ main/url__result_8h.js
@@ -39,7 +39,7 @@ var url__result_8h =
       [ "file_empty_path", "url__result_8h.html#a80c9675b4477dda8ff779545c1ad75f7add1b18bee25a37b9de731f67e11402c5", null ],
       [ "file_unsupported_path", "url__result_8h.html#a80c9675b4477dda8ff779545c1ad75f7abece6466dc75c416e679c74f16bc0f2f", null ],
       [ "not_file_url", "url__result_8h.html#a80c9675b4477dda8ff779545c1ad75f7a6c48249324054e027b248b12b6c3b12e", null ],
-      [ "file_url_host_unsupported", "url__result_8h.html#a80c9675b4477dda8ff779545c1ad75f7ae6810d2d7075ca1780fbd95c70e62287", null ],
+      [ "file_url_cannot_have_host", "url__result_8h.html#a80c9675b4477dda8ff779545c1ad75f7a638170c41e2e2dc7f761187af119d301", null ],
       [ "file_url_invalid_unc", "url__result_8h.html#a80c9675b4477dda8ff779545c1ad75f7a9c66d8c63d3ec33c819301db9d488474", null ],
       [ "file_url_not_windows_path", "url__result_8h.html#a80c9675b4477dda8ff779545c1ad75f7a2ca12369d95b6ae5b0f74b951da7eceb", null ]
     ] ],
